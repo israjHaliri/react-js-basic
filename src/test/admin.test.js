f837a81f1f626cb1772d6c33c9a6fdb530ec1379
israjHaliri <israj.haliri@gmail.com>
2019-03-18 19:35:36 +0700

    add test

--- src/test/admin.test.js
+++ src/test/admin.test.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Admin newAdminData={ {username:'' }} editAdminData={ {isEdit: false} } dispatch={()=> {}}/>, div);
+    ReactDOM.render(<Admin postData={ {firstname:'' }} putData={{firstname:'' }} dispatch={()=> {}}/>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
